9e007bb0fc385ff91eebee2de935ac67f0728dc5
Nicolás Poore <nicolaspoore@gmail.com>
2021-02-09 12:05:34 -0300

    Changed SidebarNav to functional component

--- components/Sidebar/SidebarNav.tsx
+++ components/Sidebar/SidebarNav.tsx
@@ -1,53 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Box, Fab, Tooltip } from '@material-ui/core';
 import AddCity from './AddCity';
 import { fullCitiesObject } from '../types';
 import { Add } from '@material-ui/icons';
 
-interface SidebarNavState {
-  addCityDialogOpen: boolean;
-}
+const SidebarNav: React.FC<{ cities: fullCitiesObject }> = (props): JSX.Element => {
+  const [dialogOpen, setDialogOpen] = useState(false);
 
-class SidebarNav extends Component<{ cities: fullCitiesObject }, SidebarNavState> {
-  constructor(props: { cities: fullCitiesObject } | Readonly<{ cities: fullCitiesObject }>) {
-    super(props);
-    this.state = {
-      addCityDialogOpen: false,
-    };
-  }
-
-  openAddCity = (): void => {
-    this.setState({
-      addCityDialogOpen: true,
-    });
+  const openDialog = (): void => {
+    setDialogOpen(true);
   };
 
-  closeAddCity = (): void => {
-    this.setState({
-      addCityDialogOpen: false,
-    });
+  const closeDialog = (): void => {
+    setDialogOpen(false);
   };
 
-  render(): JSX.Element {
-    return (
-      <>
-        <Box bottom="0" position="absolute" right="0">
-          <Box alignSelf="flex-end" mb={2} mr={2}>
-            <Tooltip aria-label="Add city" placement="top" title="Add city">
-              <Fab color="primary" onClick={this.openAddCity}>
-                <Add />
-              </Fab>
-            </Tooltip>
-          </Box>
+  return (
+    <>
+      <Box bottom="0" position="absolute" right="0">
+        <Box alignSelf="flex-end" mb={2} mr={2}>
+          <Tooltip aria-label="Add city" placement="top" title="Add city">
+            <Fab color="primary" onClick={openDialog}>
+              <Add />
+            </Fab>
+          </Tooltip>
         </Box>
-        <AddCity
-          cities={this.props.cities}
-          closeAddCity={this.closeAddCity}
-          open={this.state.addCityDialogOpen}
-        />
-      </>
-    );
-  }
-}
+      </Box>
+      <AddCity cities={props.cities} closeAddCity={closeDialog} open={dialogOpen} />
+    </>
+  );
+};
 
 export default SidebarNav;
