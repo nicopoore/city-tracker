126e0021e089f45d6c1d66306981a68be9501ea0
Nicolás Poore <nicolaspoore@gmail.com>
2021-02-15 14:05:47 -0300

    Small index.ts change

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,4 @@
 export { default as Map } from './Map/Map'
-export { default as Loading } from './Misc/Loading'
-export { default as SignIn } from './Misc/SignIn'
-export { default as Meta } from './Misc/Meta'
 
 export { default as Sidebar } from './Sidebar/Sidebar'
 export { default as AddCityButton } from './Sidebar/AddCityButton'
@@ -12,3 +9,7 @@ export { default as UserMenu } from './Sidebar/UserMenu'
 export { default as ShareDialog } from './Dialogs/Share'
 export { default as SignOutDialog } from './Dialogs/SignOut'
 export { default as AddCityDialog } from './Dialogs/AddCity'
+
+export { default as Loading } from './Misc/Loading'
+export { default as SignIn } from './Misc/SignIn'
+export { default as Meta } from './Misc/Meta'
