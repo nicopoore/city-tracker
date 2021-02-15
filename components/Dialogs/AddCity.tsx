541220493b0b665d67fbae1acb8a945bd16f2cae
Nicolás Poore <nicolaspoore@gmail.com>
2021-02-15 14:05:57 -0300

    Changed AddCity to functional component

--- components/Dialogs/AddCity.tsx
+++ components/Dialogs/AddCity.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { mutate } from 'swr';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
@@ -24,178 +24,131 @@ interface AddCityProps {
   cities: fullCitiesObject;
 }
 
-interface AddCityState {
-  place_id: string;
-  category_name: string;
-  category_id: string;
-}
+const AddCity: React.FC<AddCityProps> = (props): JSX.Element => {
+  const [placeId, setPlaceId] = useState('');
+  const [categoryName, setCategoryName] = useState('');
+  const [categoryId, setCategoryId] = useState('');
 
-class AddCity extends Component<AddCityProps, AddCityState> {
-  constructor(props: AddCityProps) {
-    super(props);
-    this.state = {
-      place_id: '',
-      category_name: '',
-      category_id: '',
-    };
-    this.handleCitySelected = this.handleCitySelected.bind(this);
-    this.handleSelectCategory = this.handleSelectCategory.bind(this);
-    this.handleDeselectCategory = this.handleDeselectCategory.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-  }
-
-  componentDidMount = (): void => {
-    this.setState({
-      place_id: '',
-      category_name: '',
-      category_id: '',
-    });
+  const handleCitySelected = async (e: { value: { place_id: string } }): Promise<void> => {
+    setPlaceId(e.value.place_id); // REMOVE ASYNC
   };
 
-  handleCitySelected = async (e: { value: { place_id: string } }): Promise<void> => {
-    this.setState({
-      place_id: e.value.place_id,
-    });
-  };
-
-  handleSubmit = async (option?: boolean): Promise<void> => {
+  const handleSubmit = async (option?: boolean): Promise<void> => {
     const postData = {
-      place_id: this.state.place_id,
-      category_name: this.state.category_name,
-      category_id: this.state.category_id,
+      place_id: placeId,
+      category_name: categoryName,
+      category_id: categoryId,
     };
 
     await axios.post('/api/cities', postData);
 
     if (!option) {
-      this.setState({
-        place_id: '',
-        category_name: '',
-        category_id: '',
-      });
-      this.props.closeAddCity();
+      setPlaceId('');
+      setCategoryName('');
+      setCategoryId('');
+      props.closeAddCity();
     } else if (option) {
-      const currentCat = this.state.category_name;
-      this.setState({
-        place_id: '',
-        category_name: '',
-      });
-      this.setState({
-        category_name: currentCat,
-      });
+      const currentCat = categoryName;
+      setPlaceId('');
+      setCategoryName('');
+      setCategoryName(currentCat);
     }
     mutate('/api/cities');
   };
 
-  handleSelectCategory = (category_name: string, category_id: string): void => {
-    this.setState({
-      category_name: category_name,
-      category_id: category_id,
-    });
+  const handleSelectCategory = (newCategoryName: string, newCategoryId: string): void => {
+    setCategoryName(newCategoryName);
+    setCategoryId(newCategoryId);
   };
 
-  handleDeselectCategory = (): void => {
-    this.setState({
-      category_name: '',
-      category_id: '',
-    });
+  const handleDeselectCategory = (): void => {
+    setCategoryName('');
+    setCategoryId('');
   };
 
-  handleClose = (): void => {
-    this.props.closeAddCity();
+  const handleClose = (): void => {
+    props.closeAddCity();
     setTimeout(() => {
-      this.setState({
-        place_id: '',
-        category_name: '',
-        category_id: '',
-      });
+      setPlaceId('');
+      setCategoryName('');
+      setCategoryId('');
     }, 500);
   };
 
-  render(): JSX.Element {
-    const autocompleteStyles = {
-      container: (provided: {}): {} => ({
-        ...provided,
-        margin: '2rem 0 1rem',
-      }),
-    };
-    return (
-      <Dialog
-        fullWidth
-        PaperProps={{ style: { overflowY: 'visible' } }}
-        aria-labelledby="add-city-title"
-        open={this.props.open}
-        scroll={'body'}
-        onClose={this.handleClose}
-      >
-        <DialogTitle id="add-city-title">
-          {this.state.category_name === ''
-            ? 'Add city'
-            : `Add city to "${this.state.category_name}"`}
-        </DialogTitle>
-        {this.state.category_name === '' ? (
-          <DialogContent style={{ overflowY: 'visible' }}>
-            <DialogContentText>Select the city&apos;s category</DialogContentText>
-            <List>
-              {this.props.cities.categories.map(category => (
-                <ListItem
-                  key={category['name']}
-                  button
-                  onClick={() =>
-                    this.handleSelectCategory(category['name'], category['_id'].toString())
-                  }
-                >
-                  <Room style={{ color: category['color'], marginRight: '1rem' }} />
-                  <ListItemText
-                    primary={category['name']}
-                    secondary={`${category['cities'].length} ${
-                      category['cities'].length === 1 ? 'city' : 'cities'
-                    }`}
-                  />
-                </ListItem>
-              ))}
-            </List>
-          </DialogContent>
-        ) : (
-          <DialogContent style={{ overflowY: 'visible' }}>
-            <DialogContentText>Search and select the city</DialogContentText>
-            <GooglePlacesAutocomplete
-              apiKey={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_API}
-              autocompletionRequest={{ types: ['(cities)'] }} // Limit results to cities
-              selectProps={{
-                placeholder: 'Search cities...',
-                onChange: this.handleCitySelected,
-                styles: autocompleteStyles,
-                autoFocus: true,
-              }}
-              onLoadFailed={err => console.error(err)}
-            />
-            <DialogActions>
-              <Button color="secondary" onClick={this.handleDeselectCategory}>
-                Back
-              </Button>
-              <Button
-                color="primary"
-                disabled={this.state.place_id === ''}
-                onClick={() => this.handleSubmit(true)}
+  const autocompleteStyles = {
+    container: (provided: {}): {} => ({
+      ...provided,
+      margin: '2rem 0 1rem',
+    }),
+  };
+
+  return (
+    <Dialog
+      fullWidth
+      PaperProps={{ style: { overflowY: 'visible' } }}
+      aria-labelledby="add-city-title"
+      open={props.open}
+      scroll={'body'}
+      onClose={handleClose}
+    >
+      <DialogTitle id="add-city-title">
+        {categoryName === '' ? 'Add city' : `Add city to "${categoryName}"`}
+      </DialogTitle>
+      {categoryName === '' ? (
+        <DialogContent style={{ overflowY: 'visible' }}>
+          <DialogContentText>Select the city&apos;s category</DialogContentText>
+          <List>
+            {props.cities.categories.map(category => (
+              <ListItem
+                key={category['name']}
+                button
+                onClick={() => handleSelectCategory(category['name'], category['_id'].toString())}
               >
-                Submit & add another
-              </Button>
-              <Button
-                autoFocus
-                color="primary"
-                disabled={this.state.place_id === ''}
-                onClick={() => this.handleSubmit(false)}
-              >
-                Submit
-              </Button>
-            </DialogActions>
-          </DialogContent>
-        )}
-      </Dialog>
-    );
-  }
-}
+                <Room style={{ color: category['color'], marginRight: '1rem' }} />
+                <ListItemText
+                  primary={category['name']}
+                  secondary={`${category['cities'].length} ${
+                    category['cities'].length === 1 ? 'city' : 'cities'
+                  }`}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </DialogContent>
+      ) : (
+        <DialogContent style={{ overflowY: 'visible' }}>
+          <DialogContentText>Search and select the city</DialogContentText>
+          <GooglePlacesAutocomplete
+            apiKey={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_API}
+            autocompletionRequest={{ types: ['(cities)'] }} // Limit results to cities
+            selectProps={{
+              placeholder: 'Search cities...',
+              onChange: handleCitySelected,
+              styles: autocompleteStyles,
+              autoFocus: true,
+            }}
+            onLoadFailed={err => console.error(err)}
+          />
+          <DialogActions>
+            <Button color="secondary" onClick={handleDeselectCategory}>
+              Back
+            </Button>
+            <Button color="primary" disabled={placeId === ''} onClick={() => handleSubmit(true)}>
+              Submit & add another
+            </Button>
+            <Button
+              autoFocus
+              color="primary"
+              disabled={placeId === ''}
+              onClick={() => handleSubmit(false)}
+            >
+              Submit
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      )}
+    </Dialog>
+  );
+};
 
 export default AddCity;
